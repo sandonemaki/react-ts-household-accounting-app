a775b5e9886eb120eee9a3bb82416754d519b323
sandonemaki <kamuramaki@gmail.com>
2024-12-30 20:21:38 +0900

    dataを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import { Css } from "@mui/icons-material";
 import { CssBaseline } from "@mui/material";
+import { collection, getDocs } from "firebase/firestore";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
+import { db } from "./firebase";
 import Home from "./pages/Home";
 import Nomatch from "./pages/Nomatch";
 import Report from "./pages/Report";
@@ -13,6 +15,29 @@ import type { Transaction } from "./types/index";
 
 function App() {
 	const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+	useEffect(() => {
+		const fetchTransactions = async () => {
+			try {
+				const querySnapshot = await getDocs(collection(db, "Transactions"));
+				console.log(querySnapshot);
+
+				// biome-ignore lint/complexity/noForEach: <explanation>
+				const transactionsData = querySnapshot.docs.map((doc) => {
+					// doc.data() is never undefined for query doc snapshots
+					// console.log(doc.id, " => ", doc.data());
+					return {
+						...doc.data(),
+						id: doc.id,
+					} as Transaction;
+				});
+				console.log(transactionsData);
+			} catch (error) {
+				// error
+			}
+		};
+		fetchTransactions();
+	}, []);
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
